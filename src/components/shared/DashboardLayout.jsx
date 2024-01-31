c8ef1212b2b9104fd0c8a6970be316f50f56aaba
ankan112 <ankanhalder112@gmail.com>
2024-02-01 01:03:44 +0600

    region and area page done

--- src/components/shared/DashboardLayout.jsx
+++ src/components/shared/DashboardLayout.jsx
@@ -1,14 +1,14 @@
-import { Layout, Menu, Button, theme, Breadcrumb } from "antd";
+import { Layout, Menu, theme } from "antd";
 import { Footer } from "antd/es/layout/layout";
 import logo from "../../assets/FieldXDashboard.svg";
 import location from "../../assets/location.svg";
 import { Link, Outlet } from "react-router-dom";
 import user from "../../assets/user.png";
 import arrow from "../../assets/chevron-down.svg";
-const { Header, Sider, Content } = Layout;
+const { Header, Sider } = Layout;
 const DashboardLayout = () => {
   const {
-    token: { colorBgContainer, borderRadiusLG },
+    token: { colorBgContainer },
   } = theme.useToken();
   return (
     <Layout>
@@ -134,79 +134,6 @@ const DashboardLayout = () => {
         </Header>
         <Outlet />
 
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <div>
-            <h2
-              style={{
-                margin: "32px 0 8px 24px",
-                fontSize: "20px",
-                fontWeight: 700,
-                color: "#0C1D37",
-              }}
-            >
-              Region List
-            </h2>
-            <Breadcrumb
-              style={{
-                marginLeft: "24px",
-                marginBottom: "8px",
-              }}
-              separator=">"
-              items={[
-                {
-                  title: "Geo",
-                },
-                {
-                  title: "Geo List",
-                  href: "",
-                },
-              ]}
-            />
-          </div>
-          <Button
-            style={{
-              marginRight: "20px",
-            }}
-            type="primary"
-          >
-            Create New
-          </Button>
-        </div>
-        <Content
-          style={{
-            margin: "24px 16px",
-            padding: 24,
-            minHeight: 706,
-            background: colorBgContainer,
-            borderRadius: borderRadiusLG,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <div>
-            <p
-              style={{
-                fontSize: "20px",
-                fontWeight: 400,
-                color: "#9FA3A6",
-                textAlign: "center",
-              }}
-            >
-              Currently you have no Data. <br />
-              For next step first{" "}
-              <Link to={"/"} style={{ color: "#556EE6" }}>
-                Create Region
-              </Link>
-            </p>
-          </div>
-        </Content>
         <Footer
           style={{
             textAlign: "center",
--- src/pages/dashboard/Area.jsx
+++ src/pages/dashboard/Area.jsx
@@ -1,7 +1,85 @@
+import { Breadcrumb, Button, theme, Layout } from "antd";
+import { Link } from "react-router-dom";
+const { Content } = Layout;
 const Area = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
   return (
     <div>
-      <h1>area</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <h2
+            style={{
+              margin: "32px 0 8px 24px",
+              fontSize: "20px",
+              fontWeight: 700,
+              color: "#0C1D37",
+            }}
+          >
+            Area List
+          </h2>
+          <Breadcrumb
+            style={{
+              marginLeft: "24px",
+              marginBottom: "8px",
+            }}
+            separator=">"
+            items={[
+              {
+                title: "Geo",
+              },
+              {
+                title: "Geo List",
+                href: "",
+              },
+            ]}
+          />
+        </div>
+        <Button
+          style={{
+            marginRight: "20px",
+          }}
+          type="primary"
+        >
+          Create New
+        </Button>
+      </div>
+      <Content
+        style={{
+          margin: "24px 16px",
+          padding: 24,
+          minHeight: 706,
+          background: colorBgContainer,
+          borderRadius: borderRadiusLG,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <p
+            style={{
+              fontSize: "20px",
+              fontWeight: 400,
+              color: "#9FA3A6",
+              textAlign: "center",
+            }}
+          >
+            Currently you have no Data. <br />
+            For next step first{" "}
+            <Link to={"/"} style={{ color: "#556EE6" }}>
+              Create Area
+            </Link>
+          </p>
+        </div>
+      </Content>
     </div>
   );
 };
