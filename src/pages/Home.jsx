bd42e1b5c044b4e0f7d71658ae0c0c3a30416528
ankan112 <ankanhalder112@gmail.com>
2024-02-01 21:03:15 +0600

    faq section done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import Faq from "../components/Home/FAQ";
 import Partners from "../components/Home/Partners";
 
 const Home = () => {
   return (
     <div className="main-container">
+      <Faq />
       <Partners />
     </div>
   );
